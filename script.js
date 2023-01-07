2df96bfdf76a4a5b68ea6833a4479e954cec59dc
treezyxd <thalesmaltaxd@gmail.com>
2023-01-07 03:37:00 -0300

    js integrated and box shadow finished

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-function selecionarEscolha(thisChoose) {
-  console.log(thisChoose);
-  // thisChoose.classList.toggle('vermelho');
+function selecionarComida(thisChoose) {
+  const produtoSelecionado = document.querySelector('.food-list .selecionado');
+
+  if(produtoSelecionado !== null) {
+    produtoSelecionado.classList.remove('selecionado');
+  } else {
+    thisChoose.classList.add('selecionado');
+  }
+}
+
+function selecionarBebida(thisChoose) {
+  const produtoSelecionado = document.querySelector('.drink-list .selecionado');
+
+  if(produtoSelecionado !== null) {
+    produtoSelecionado.classList.remove('selecionado');
+  } else {
+    thisChoose.classList.add('selecionado');
+  }
+}
+
+function selecionarSobremesa(thisChoose) {
+  const produtoSelecionado = document.querySelector('.dessert-list .selecionado');
+  const iconeFinalizado = document.querySelector('.dessert-product .escondido');
+
+  if(produtoSelecionado !== null) {
+    produtoSelecionado.classList.remove('selecionado');
+    
+  } else {
+    thisChoose.classList.add('selecionado');
+  }
+
 }
