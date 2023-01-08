e3e9aef84a7c97e44f6f6ae37750cc825bd06363
treezyxd <thalesmaltaxd@gmail.com>
2023-01-08 16:10:18 -0300

    icon-check arrumado

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
-function selecionarComida(thisChoose) {
+function selecionarComida(choosedThis) {
   const produtoSelecionado = document.querySelector('.food-list .selecionado');
 
   if(produtoSelecionado !== null) {
     produtoSelecionado.classList.remove('selecionado');
   } else {
-    thisChoose.classList.add('selecionado');
+    choosedThis.classList.add('selecionado');
   }
 }
 
-function selecionarBebida(thisChoose) {
+function selecionarBebida(choosedThis) {
   const produtoSelecionado = document.querySelector('.drink-list .selecionado');
 
   if(produtoSelecionado !== null) {
     produtoSelecionado.classList.remove('selecionado');
+    // esconderCheck();
   } else {
-    thisChoose.classList.add('selecionado');
+    choosedThis.classList.add('selecionado');
+    // mostrarCheck();
   }
 }
 
-function selecionarSobremesa(thisChoose) {
+function selecionarSobremesa(choosedThis) {
   const produtoSelecionado = document.querySelector('.dessert-list .selecionado');
-  const iconeFinalizado = document.querySelector('.dessert-product .escondido');
+  const iconFinalizado = document.querySelector('.dessert-product .escondido');
 
   if(produtoSelecionado !== null) {
     produtoSelecionado.classList.remove('selecionado');
-    
-  } else {
-    thisChoose.classList.add('selecionado');
   }
+  choosedThis.classList.add('selecionado');
 
+  if (iconFinalizado !== null) {
+    iconFinalizado.classList.remove('escondido');
+  }
+  iconFinalizado.classList.add('escondido');
+}
+
+function selecionarProduto(pedido) {
+  const selecionadoAnterior = pedido.parentElement.querySelector('.selecionado');
+  
+  if(selecionadoAnterior !== null) {
+    selecionadoAnterior.classList.remove('selecionado');
+  }
+  pedido.classList.add('selecionado'); 
+  document.querySelector('.btn').add
+}
+
+function liberarPedido(choosedThis) {
+  const liberarBotao = document.querySelector('.finish-order .btn-selecionado');
+  const comidaSelecionada = document.querySelector('.food-list selecionado');
+
+  if(comidaSelecionada !== null) {
+    liberarBotao.classList.add('btn-selecionado');
+  }
 }
