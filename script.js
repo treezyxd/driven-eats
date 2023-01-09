e7832712efafd42f29bb46e86518b10d81d1131f
treezyxd <thalesmaltaxd@gmail.com>
2023-01-08 21:51:17 -0300

    finished w/o wpp

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-function selecionarProduto(pedido) {
-  const selecionadoAnterior = pedido.parentElement.querySelector('.selecionado');
-  
-  if(selecionadoAnterior !== null) {
-    selecionadoAnterior.classList.remove('selecionado');
-  }
-  pedido.classList.add('selecionado'); 
-  document.querySelector('.btn').add
-}
+let comidaSelecionada;
 
-function liberarPedido(choosedThis) {
-  const liberarBotao = document.querySelector('.finish-order .btn-selecionado');
-  const comidaSelecionada = document.querySelector('.food-list selecionado');
+function selecionarComida(choosedThis) {
+  comidaSelecionada = document.querySelector('.food-list .selecionado');
 
   if(comidaSelecionada !== null) {
-    liberarBotao.classList.add('btn-selecionado');
+    comidaSelecionada.classList.remove('selecionado');
+  }
+  choosedThis.classList.add('selecionado');
+}
+
+let bebidaSelecionada;
+
+function selecionarBebida(choosedThis) {
+  bebidaSelecionada = document.querySelector('.drink-list .selecionado');
+
+  if(bebidaSelecionada !== null) {
+    bebidaSelecionada.classList.remove('selecionado');
+  }
+  choosedThis.classList.add('selecionado');
+}
+
+let sobremesaSelecionada;
+
+function selecionarSobremesa(choosedThis) {
+  sobremesaSelecionada = document.querySelector('.dessert-list .selecionado');
+
+  if(sobremesaSelecionada !== null) {
+    sobremesaSelecionada.classList.remove('selecionado');
+  }
+  choosedThis.classList.add('selecionado');
+}
+
+function verificaPedido() {
+  const mudarBotao = document.querySelector('.btn');
+  
+  if( comidaSelecionada !== undefined && bebidaSelecionada !== undefined && sobremesaSelecionada !== undefined) {
+    mudarBotao.removeAttribute('disabled');
+    mudarBotao.classList.add('btn-selecionado');
+    mudarBotao.innerHTML = "Fechar pedido";
   }
 }
